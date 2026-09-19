d83b6b5d4bd72692d4bda47ec298a05888cef9d5
Ronny <RonnyWu.pe@gmail.com>
2026-09-19 23:58:09 +0800

    test(collections): add EditMode test assembly

---
+++ Packages/com.ronny.grow/Tests/Editor/SmokeTests.cs
@@ -0,0 +1,15 @@
+// Copyright (c) 2026 Ronny Wu
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+
+using NUnit.Framework;
+
+namespace Grow.Tests {
+    [TestFixture]
+    public sealed class SmokeTests {
+        [Test]
+        public void TestAssembly_IsDiscovered() {
+            Assert.Pass();
+        }
+    }
+}
